9731c54254b3ed09a2b1155be0b9afd776652c62
Tigran124 <tigran.h.988@gmail.com>
2024-01-12 17:54:43 +0400

    changes completed

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,26 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator comparator = new ListComparator();
+        sourceList.sort(comparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int x = Integer.parseInt(a);
+        int y = Integer.parseInt(b);
+        int aVal = 5 * (x * x) + 3;
+        int bVal = 5 * (y * y) + 3;
+        if (aVal == bVal){
+            if (x > y){
+                return 1;
+            }else {
+                return -1;
+            }
+        } else if (aVal > bVal) {
+            return 1;
+        }else return -1;
     }
 }
